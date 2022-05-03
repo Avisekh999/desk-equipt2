a06151f4d8d1c5fcdf7170a45dceca46c47bad37
Avisekh <avisekhgurung8578@gmail.com>
2022-05-03 22:26:21 +0530

    first-commit

---
+++ precache-manifest.27eade1142158330be86cf88180c5f28.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "3655d8cdc31791d69f76",
+    "url": "/static/css/main.fea284a1.chunk.css"
+  },
+  {
+    "revision": "3655d8cdc31791d69f76",
+    "url": "/static/js/main.3655d8cd.chunk.js"
+  },
+  {
+    "revision": "6efb1481cc4ab4ae9077",
+    "url": "/static/js/1.6efb1481.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5bc1ae7aa3ad7b7d6ac7a96a0dd5ec3d",
+    "url": "/static/media/break.5bc1ae7a.svg"
+  },
+  {
+    "revision": "77707b71fc3373ea4eb08d792486dc86",
+    "url": "/static/media/tick.77707b71.svg"
+  },
+  {
+    "revision": "2975b9bad1c6e2f1574352c2a28d19ac",
+    "url": "/static/media/logoNew.2975b9ba.svg"
+  },
+  {
+    "revision": "79a364cd8ad6cd18111c6dd58104e787",
+    "url": "/static/media/businesspeople-meeting-office-working.79a364cd.jpg"
+  },
+  {
+    "revision": "49b00126141cac4afb6832483c530d08",
+    "url": "/static/media/shutterstock_1.49b00126.jpg"
+  },
+  {
+    "revision": "0f5a97eebd3b8c13a4e93644ef895cf5",
+    "url": "/static/media/time.0f5a97ee.svg"
+  },
+  {
+    "revision": "933c6e4393a83454388178fcebe083b8",
+    "url": "/static/media/focus.933c6e43.svg"
+  },
+  {
+    "revision": "1350eaf5f8e94d9caec6b955b959f86f",
+    "url": "/static/media/touchpoint.1350eaf5.svg"
+  },
+  {
+    "revision": "00a30c8354d738c73f929d4a2f79f733",
+    "url": "/static/media/communication.00a30c83.svg"
+  },
+  {
+    "revision": "441cc7bc8aeac08504125462adc63c03",
+    "url": "/static/media/decision-making.441cc7bc.svg"
+  },
+  {
+    "revision": "55e3c2e6f391a4bb0b31285f13250a25",
+    "url": "/static/media/shutterstock_3.55e3c2e6.jpg"
+  },
+  {
+    "revision": "acaae874ce87d97356cede3359daf3f9",
+    "url": "/static/media/quote.acaae874.svg"
+  },
+  {
+    "revision": "b3cb4fca2e3b87b245af760088bc31f0",
+    "url": "/static/media/bulb.b3cb4fca.svg"
+  },
+  {
+    "revision": "d78df1d068bc0e257864a419f4f5a47e",
+    "url": "/static/media/clock.d78df1d0.svg"
+  },
+  {
+    "revision": "f3a50eaeb57ce651177852703423cb07",
+    "url": "/static/media/Group 394.f3a50eae.png"
+  },
+  {
+    "revision": "3be39d75a1c8fb242cc959d19b07f4ba",
+    "url": "/static/media/mac2.3be39d75.png"
+  },
+  {
+    "revision": "929727411c166d729ffe9f7ea88fecbb",
+    "url": "/static/media/newecom.92972741.png"
+  },
+  {
+    "revision": "567c55efa940450139716531d93d50a2",
+    "url": "/static/media/shutterstock_2.567c55ef.jpg"
+  },
+  {
+    "revision": "898d52bfbbe6540c61e671ce2d10633c",
+    "url": "/static/media/settings.898d52bf.svg"
+  },
+  {
+    "revision": "0fab8e811e5999eacc456e37ad53de7e",
+    "url": "/static/media/chart.0fab8e81.svg"
+  },
+  {
+    "revision": "dad33304bbeb4c7f2e90288da0136b4e",
+    "url": "/static/media/Group.dad33304.svg"
+  },
+  {
+    "revision": "0e970f8c9164f768d4e25a63af15f4e8",
+    "url": "/static/media/eliminate waste.0e970f8c.svg"
+  },
+  {
+    "revision": "5770a2b58c4b5354db29c306a31cd7b8",
+    "url": "/static/media/eliminate-waste=green.5770a2b5.svg"
+  },
+  {
+    "revision": "73acd147fa2a0cb61136f593cc826fef",
+    "url": "/static/media/decision.73acd147.svg"
+  },
+  {
+    "revision": "d30de0ad90d525224b78d446ed77e534",
+    "url": "/static/media/accelerate job.d30de0ad.svg"
+  },
+  {
+    "revision": "1d7f4180d4341f892f70dd97d67d8ede",
+    "url": "/static/media/proposal.1d7f4180.svg"
+  },
+  {
+    "revision": "522bb7d2ef95aa782e722e5c73009b22",
+    "url": "/static/media/rental program.522bb7d2.svg"
+  },
+  {
+    "revision": "2b37344bf0fb896ae2b8e9cc88395272",
+    "url": "/static/media/cart.2b37344b.svg"
+  },
+  {
+    "revision": "e824aa923f4f2add63c22adf65254804",
+    "url": "/static/media/inventory.e824aa92.svg"
+  },
+  {
+    "revision": "beac3ad3a3023f600720109d02edda97",
+    "url": "/static/media/image 4.beac3ad3.png"
+  },
+  {
+    "revision": "721aa89ce6679fd61012a73e4efe491c",
+    "url": "/static/media/image 7.721aa89c.png"
+  },
+  {
+    "revision": "1c1ce33e52afa8f166d3710e82d56a10",
+    "url": "/index.html"
+  }
+];
